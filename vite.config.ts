f8c70ae1bbabc035fd3d5673dde38516e1c583bd
aydengulf-eng <aydengulf@gmail.com>
2026-09-15 07:46:49 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { readExecutionProfile } from "./scripts/execution-profile.mjs";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "00ca04ef-475c-4cc6-adbc-6d1b9fc4cd32";
 
 const { d1, r2 } = hostingConfig;
 
